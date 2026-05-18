4c0c5770931d3e177ebf02b81bbb6089bf5473d6
Thainá Santos Alencar <167568065+Thatacorekook902@users.noreply.github.com>
2026-05-18 07:33:45 -0300

    Update script.js

---
+++ public/script.js
@@ -0,0 +1,21 @@
+const { json } = require("express");
+
+document.getElementById("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        nome: document.getElementById("nome").value,
+        cpf: document.getElementById("cpf").value
+    };
+
+    const res = await fetch("/cadastro", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const responseJson = await res.json();
+    alert(JSON.stringify(responseJson));
+});
